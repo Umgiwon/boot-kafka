2cf34678b5a4dd1a0b04845be9ab685ca3fd5877
umgiwon <ugwzzang@gmail.com>
2025-04-30 11:43:14 +0900

    # init

---
+++ src/test/java/com/one/bootkafka/BootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.bootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
